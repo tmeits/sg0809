25863384fc7990854e216b953a12f9314952dccb
ilynva <ilynva@0a1a99f2-b6a5-11dd-aa87-4b319d2fb4de>
2008-12-09 05:51:26 +0000

    arstan.tcl принимает параметр timeout и имя файла с данными.

--- trunk/arstan.tcl
+++ trunk/arstan.tcl
@@ -14,29 +14,41 @@ exec tclsh "$0"  ${1+"$@"}
 # This is required to declare that we will use Expect
 package require Expect
 
-#Правила использования скрипта
-#В дальнейшем через параметры предполагается управлять генерацией выходного файла
-#set arstan [lindex $argv 0]
-#if {![string is integer -strict $yux] && $yux ne "-myarg"} {
-#   puts stderr "usage: $argv0 <argument> | -myarg"
-#   exit 1
-#}
+proc usage {} {
+    puts stderr "usage: $::argv0 <timeInSecs> <DataFileName>"
+    exit 1
+}
+proc delrez {dirpatch} {
+# Удаляем файлы результатов предыдущих запусков скрипта 
+if ![catch {exec cmd.exe /c del $dirpatch}] {
+  return 1
+} else {
+ return 2
+ }
+}
+if {$argc < 2} { usage }
 
+# timeout value is first to be passed in
+# Если expect не получит нужную команду он прерывается. -1 ждать вечно. 0 - не ждать
+set timeout [lindex $argv 0]
+if {![string is integer -strict $timeout]} {set timeout 10}
+set datafile [lindex $argv 1]
+# don't echo user output
+exp_log_user 0
 
+set dirpatch {ilynva*.*}
+delrez $dirpatch
+delrez {zz*}
 # Переменные
-set file "russ001.rwl"
+#set datafile {sg0809-dd.rwl}
 
-
-#Если expect не получит нужную команду он прерывается. -1 ждать вечно. 0 - не ждать
-set timeout 1
-
-#spawn cmd.exe /c {c:\shishov\test\run-ss.bat}
+# Запуск проргарммы расчета
 spawn  {c:\shishov\ARSTAN.EXE}
 #ждем 
 expect {"=>"}
 send "ilynva\r"
 expect {"=>"}
-send "sg0809-dd.rwl\r"
+send "$datafile\r"
 expect {"=>"}
 send "\r"
 expect {"=>"}
@@ -46,8 +58,9 @@ send "\r"
 expect {"=>"}
 send "\r"
 
-
+#exp_close
 expect eof
+
 
 #flush ""
 
