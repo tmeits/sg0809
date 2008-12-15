c719525ee9f65b89d20f11355812820dab1915fa
ilynva <ilynva@0a1a99f2-b6a5-11dd-aa87-4b319d2fb4de>
2008-12-15 10:00:35 +0000

    что получилось

--- select-fd.tcl
+++ select-fd.tcl
@@ -3,13 +3,17 @@
 exec tclsh "$0"  ${1+"$@"}
 
 #
-# Тестовая версия скрипта с использование expect
-#
+# Версия скрипта с использование expect
+# Предлагает пользователю выбрать из списка файл данных и передает эту информацию скрипту расчета 
 # Usage:
-#	tclsh s-arstan.tcl
+#	tclsh select-fd.tcl
 #	ilynva@ на гугле
 #	12.11.2008
 #
+
+proc cfl {} {
+return 1;
+}
 
 set dir [file dirname [info script]]
 lappend auto_path $dir [file join $dir ../../cgi-bin]
@@ -23,69 +27,38 @@ Content-Type: text/html; charset=windows-1251
 Expires: 0
 Pragma: no-cache\n"
 
-puts "<h1>Выполняется sg0809-d1.tcl</h1>"
-puts "<pre>$errorInfo</pre>"
-puts "Печатем авто-путь (нужно для отладки)<br>"
-puts $auto_path
-
-# Удаляем файлы результатов предыдущих запусков скрипта 
-if ![catch {exec cmd.exe /c del {C:\TCLHTTPD3.5.1\htdocs\arstan\rez\ilynva*.*}}] {
-  puts "<br>OK\n"
-} else {
- puts "<br> Удалять нечего!\n"
- }
-if ![catch {exec cmd.exe /c del {C:\TCLHTTPD3.5.1\htdocs\cgi-bin\arstan\zz*.*}}] {
- puts "OK\n"
-} else {
- puts "<br> Удалять нечего!\n"
-}
-
-#На данный момент выбор файла пользователя представлен сриском из которого он делает выбор
-
-puts "<h4>Расчет выполняется c данными $cgi(vpl)!!!! Ждите!</h4>"
-
-set copydatafile1 {C:\TCLHTTPD3.5.1\htdocs\sd0809\arstan\dat}
-set copydatafile2 "$cgi(vpl)"
-set copydatafile "$copydatafile1\\$copydatafile2"
-puts $copydatafile
-
-#Копируем файл данных
-if ![catch {exec cmd.exe /c copy /y /a $copydatafile {C:\TCLHTTPD3.5.1\htdocs\cgi-bin\arstan\sg0809-dd.rwl}}] {
- puts "<br>Файл данных $cgi(vpl) подготовлен для расчета...\n"
-} else {
- puts "<br> Ошибка копирования файла $cgi(vpl)!\n"
-}
-
-exec tclsh.exe {C:\TCLHTTPD3.5.1\htdocs\cgi-bin\arstan\arstan.tcl} 1 sg0809-dd.rwl
-
-exec cmd.exe /c copy /y /a {C:\TCLHTTPD3.5.1\htdocs\cgi-bin\arstan\ilynva*.*} {C:\TCLHTTPD3.5.1\htdocs\sd0809\arstan\rez\*.*}
-
-if ![catch {exec cmd.exe /c del {C:\TCLHTTPD3.5.1\htdocs\cgi-bin\arstan\ilynva*.*}}] {
-  puts "<br>OK\n"
-} else {
- puts "<br> Удалять нечего!\n"
- }
-if ![catch {exec cmd.exe /c del {C:\TCLHTTPD3.5.1\htdocs\cgi-bin\arstan\zz*.*}}] {
- puts "OK\n"
-} else {
- puts "<br> Удалять нечего!\n"
-}
-
-puts "<br><h4> Выполнение успешно завершенно!!!</h4>\n<br><hr>"
-# Копируем файл данных для yux.exec
-set copydatafileyux {C:\TCLHTTPD3.5.1\htdocs\sd0809\arstan\rez\ilynvars.crn}
-if ![catch {exec cmd.exe /c copy /y /a $copydatafileyux {C:\TCLHTTPD3.5.1\htdocs\cgi-bin\arstan}}] {
- puts "<br>Файл данных ilynvars.crn подготовлен для расчета программой YUX...\n"
-} else {
- puts "<br> Ошибка копирования файла ilynvars.crn !\n"
-}
-
-puts "<FORM ACTION=http://192.168.10.3:8015/cgi-bin/arstan/s-yux.tcl METHOD=POST>"
-puts "<INPUT TYPE=\"submit\" NAME=\"calcyux\" VALUE=\"Провести расчет YUX\">"
-puts "</FORM>"
+puts "<h2>Интерактивная дендроклиматическая система</h2>"
 puts "<hr>"
-puts "<br><a href='http://192.168.10.3:8015/sd0809/arstan/rez/'>Загрузить результат</a>\n"
-puts "<br><a href='http://192.168.10.3:8015/sd0809/arstan/arstan.html'>Повторить</a>\n"
-#flush ""
+
+puts "<FORM ACTION=\"/cgi-bin/arstan/sg0809-d1.tcl\" METHOD=\"POST\">"
+#Получаем список файлов, которые соответсвуют координатам
+
+set coord-file-list [cfl]
+
+# Создаем таблицу ассоциирующую координаты с именами файлов с данными
+puts "<table border = '1'> "
+for {set x 0} {$x<10} {incr x} {
+    puts "<tr> <td>russ190.rwl</td>  <td>....</td> <td>....</td></tr>"
+}
+puts "</table><p>"
+
+# Создаем список с именами файлов с данными
+puts "<select name='vpl' > "
+for {set x 0} {$x<10} {incr x} {
+    puts "<option> russ190.rwl"
+}
+puts "</select>"
+puts "<p>"
+puts "<INPUT TYPE=\"submit\" NAME=\"calcarstan\" VALUE=\"Провести расчет\">"
+puts "</FORM>"
+
+puts "<hr>"
+#Получить текущюю дату в виде строки
+set s1time [clock seconds]
+#set s2time [clock seconds]
+#puts "$s1time : $s2time"
+
+set st [clock format $s1time]
+puts "$st"
 
 Cgi_Tail
